0677e49f55829a29778b66f188132b74d8dc1779
HoangPhung-11 <80802358+HoangPhung-11@users.noreply.github.com>
2024-08-05 15:14:13 +0700

    Left-column Champion Completed

--- apps/frontend/ZZZOptimizer/src/components/CharacterPage/CharacterColumns.jsx
+++ apps/frontend/ZZZOptimizer/src/components/CharacterPage/CharacterColumns.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import CharacterLeftColumn from "./CharacterLeftColumn";
 import CharacterMainColumn from "./CharacterMainColumn";
+import "./character.css";
 
 function CharacterColumns() {
   return (
-    <div className="row">
+    <div className="row character-columns-holder">
       <div className="col-lg-4">
         <CharacterLeftColumn />
       </div>
--- apps/frontend/ZZZOptimizer/src/components/CharacterPage/CharacterLeftColumn.jsx
+++ apps/frontend/ZZZOptimizer/src/components/CharacterPage/CharacterLeftColumn.jsx
@@ -1,7 +1,81 @@
 import React from "react";
+import fireLogo from "../../assets/logo/Icon_Fire.jpg";
+import electricLogo from "../../assets/logo/Icon_Electric.jpg";
+import etherLogo from "../../assets/logo/Icon_Ether.jpg";
+import iceLogo from "../../assets/logo/Icon_Ice.jpg";
+import physicalLogo from "../../assets/logo/Icon_Physical.jpg";
+import agentRinaLogo from "../../assets/logo/agents/Agent_Rina_Icon.jpg";
+import "./character.css";
 
 function CharacterLeftColumn() {
-  return <div>Left Column</div>;
+  return (
+    <div className="row left-table">
+      <div className="search-bar">
+        <input type="text" placeholder="Search a champion"></input>
+      </div>
+
+      <div class="filter-bar">
+        <button type="button" className="btn btn-secondary">
+          <img alt="Electric Logo" src={electricLogo} className="nav-icon" />
+        </button>
+
+        <button type="button" className="btn btn-secondary">
+          <img alt="Fire Logo" src={fireLogo} className="nav-icon" />
+        </button>
+
+        <button type="button" className="btn btn-secondary">
+          <img alt="Physical Logo" src={physicalLogo} className="nav-icon" />
+        </button>
+
+        <button type="button" className="btn btn-secondary">
+          <img alt="Ice Logo" src={iceLogo} className="nav-icon" />
+        </button>
+
+        <button type="button" className="btn btn-secondary">
+          <img alt="Ether Logo" src={etherLogo} className="nav-icon" />
+        </button>
+      </div>
+
+      <div class="champion-grid">
+        <div class="champion">
+          <img alt="Rina Logo" src={agentRinaLogo} className="nav-icon" />
+          <span class="champion-name"> Alexandrina Sebastiane</span>
+        </div>
+        <div class="champion">
+          <img alt="Rina Logo" src={agentRinaLogo} className="nav-icon" />
+          <span class="champion-name">Alexandrina Sebastiane</span>
+        </div>
+        <div class="champion">
+          <img alt="Rina Logo" src={agentRinaLogo} className="nav-icon" />
+          <span class="champion-name">Alexandrina Sebastiane</span>
+        </div>
+        <div class="champion">
+          <img alt="Rina Logo" src={agentRinaLogo} className="nav-icon" />
+          <span class="champion-name">Alexandrina Sebastiane</span>
+        </div>
+        <div class="champion">
+          <img alt="Rina Logo" src={agentRinaLogo} className="nav-icon" />
+          <span class="champion-name">Alexandrina Sebastiane</span>
+        </div>
+        <div class="champion">
+          <img alt="Rina Logo" src={agentRinaLogo} className="nav-icon" />
+          <span class="champion-name">Alexandrina Sebastiane</span>
+        </div>
+        <div class="champion">
+          <img alt="Rina Logo" src={agentRinaLogo} className="nav-icon" />
+          <span class="champion-name">Alexandrina Sebastiane</span>
+        </div>
+        <div class="champion">
+          <img alt="Rina Logo" src={agentRinaLogo} className="nav-icon" />
+          <span class="champion-name">Alexandrina Sebastiane</span>
+        </div>
+        <div class="champion">
+          <img alt="Rina Logo" src={agentRinaLogo} className="nav-icon" />
+          <span class="champion-name">Alexandrina Sebastiane</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CharacterLeftColumn;
