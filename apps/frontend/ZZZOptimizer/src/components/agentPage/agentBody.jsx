e83bda2d6a14480a0292d83f5ed1837cb9ef867e
trinhminh11 <trinhtheminh1104@gmail.com>
2024-08-06 10:14:17 +0700

    Export class instead of function, add AgentBaseStat in json format

--- apps/frontend/ZZZOptimizer/src/components/agentPage/agentBody.jsx
+++ apps/frontend/ZZZOptimizer/src/components/agentPage/agentBody.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import AgentLocalDatabase from "./agentLocalDatabase";
-import AgentProcessing from "../../../../../backend/components/agentProcessing";
+import AgentInfo from "../../../../../backend/components/agents/agentInfo";
 import "./agent.css";
 
 function agentBody() {
   return (
     <div className="row agent-body">
       <div className="col-lg-3">
-        <AgentProcessing />
+        <AgentInfo />
       </div>
       <div className="col-lg-9">
         <AgentLocalDatabase />
