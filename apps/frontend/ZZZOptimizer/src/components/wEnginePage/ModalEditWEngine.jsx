4ea6ec89ef422137057d2a0415deee523617fc8c
HoangPhung-11 <80802358+HoangPhung-11@users.noreply.github.com>
2024-08-21 22:30:54 +0700

    W-Engine Modal Updated

--- apps/frontend/ZZZOptimizer/src/components/wEnginePage/ModalEditWEngine.jsx
+++ apps/frontend/ZZZOptimizer/src/components/wEnginePage/ModalEditWEngine.jsx
@@ -1,18 +1,22 @@
 import { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import parse from "html-react-parser";
 import "./wEngine.css";
 
 export default function ModalEditEngine(props) {
   const { show, handleClose, dataWEngineEdit, removeWEngine } = props;
+  let passiveDescription = dataWEngineEdit.passive;
   return (
     <div>
       <Modal
-        className="modal-container"
+        className="wEngine-modal-container"
         show={show}
         onHide={handleClose}
         backdrop={"static"}
         keyboard={false}
+        size="lg"
+        centered
       >
         <Modal.Header className="modal-header">
           <h4>{dataWEngineEdit.name}</h4>
@@ -24,7 +28,37 @@ export default function ModalEditEngine(props) {
           </button>
         </Modal.Header>
 
-        <Modal.Body className="modal-body"></Modal.Body>
+        <Modal.Body className="wEngine-modal-body">
+          <div className="col-lg-4">
+            <div className={"wEngine-left-img agent-" + dataWEngineEdit.rank}>
+              <img src={dataWEngineEdit.nameIcon} alt="" />
+            </div>
+          </div>
+          <div className="col-lg-8">
+            <div>{parse(String(passiveDescription))}</div>
+            <div>
+              <table class="table table-bordered table-dark stat-table">
+                <thead>
+                  <tr>
+                    <td scope="col" colspan="2">
+                      Main Stat
+                    </td>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                  </tr>
+                  <tr>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </Modal.Body>
         <Modal.Footer className="modal-footer no-border">
           <Button variant="secondary" onClick={handleClose}>
             Close
