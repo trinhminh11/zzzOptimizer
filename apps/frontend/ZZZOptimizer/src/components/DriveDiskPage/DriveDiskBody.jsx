d53999bf72582244b6801980ee6810efac00267e
HoangPhung-11 <80802358+HoangPhung-11@users.noreply.github.com>
2024-09-17 15:19:25 -0400

    update CSS

--- apps/frontend/ZZZOptimizer/src/components/DriveDiskPage/DriveDiskBody.jsx
+++ apps/frontend/ZZZOptimizer/src/components/DriveDiskPage/DriveDiskBody.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "./driveDisk.css";
 
 function DriveDiskBody() {
-  return <div>DriveDiskBody</div>;
+  return (
+    <div className="drive-disc-container">
+      <button className="add-disk-btn">Add a Drive Disk</button>
+    </div>
+  );
 }
 
 export default DriveDiskBody;
